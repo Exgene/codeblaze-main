6835d36562bb786d5d7946af36ded9259119bb7a
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2023-12-09 12:32:34 +0530

    responsiveness

--- src/lib/components/authcheck.svelte
+++ src/lib/components/authcheck.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-	import { user } from '$lib/firebase/firebase';
-	import { Button } from '$lib/components/ui/button';
-	import * as Card from '$lib/components/ui/card';
-	import SignInOut from './signin-out.svelte';
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import { user } from "$lib/firebase/firebase";
+  import SignInOut from "./signin-out.svelte";
 </script>
 
 {#if $user}
-	<slot />
+  <slot />
 {:else}
-	<Card.Root class="mt-10 w-[500px] mx-auto">
-		<Card.Header class="space-y-1">
-			<Card.Title class="text-2xl">You aren't logged in</Card.Title>
-			<Card.Description>You need to be logged in to view this page. If you already have an account you can login else click the SignUp button to create an account.</Card.Description>
-		</Card.Header>
+  <Card.Root class="mt-10 w-[500px] mx-auto">
+    <Card.Header class="space-y-1">
+      <Card.Title class="text-2xl">You aren't logged in</Card.Title>
+      <Card.Description
+        >You need to be logged in to view this page.
+      </Card.Description>
+    </Card.Header>
 
-		<Card.Content class="flex flex-row flex-wrap justify-center gap-4">
-			<SignInOut />
-		</Card.Content>
-	</Card.Root>
-{/if}    
+    <Card.Content class="flex flex-row flex-wrap justify-center gap-4">
+      <SignInOut />
+    </Card.Content>
+  </Card.Root>
+{/if}
--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <nav class="border h-20 flex items-center justify-center gap-x-10">
-  <div class="text-3xl font-medium absolute left-6">BidBazaar</div>
-  <div class="gap-x-6 flex text-lg font-bold text-slate-500 ">
+  <div class="lg:text-3xl font-medium absolute left-6 text-base">BidBazaar</div>
+  <div class="gap-x-6 flex text-lg font-bold text-slate-500">
     <a href="/" class="hover:text-black duration-150">Current</a>
     <a href="/history" class="hover:text-black duration-150">History</a>
   </div>
